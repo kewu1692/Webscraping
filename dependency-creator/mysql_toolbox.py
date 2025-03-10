72c91472d0d8de7ef59492f3141ed8131ea364fd
Jessie Chen <jchen000311@gmail.com>
2025-03-10 16:57:52 -0400

    executing query based on validation results

--- dependency-creator/mysql_toolbox.py
+++ dependency-creator/mysql_toolbox.py
@@ -35,36 +35,44 @@ def execute_query_with_replace(cursor, query, replace_map):
 
 # validate_replace_by_query takes query and dict and validates if the dict is good
 def validate_replace_by_query(query, replace_map):
+    # check input type
     if not isinstance(query, str):
         raise TypeError(f"Expected string, got {type(query).__name__}")
     if not isinstance(replace_map, dict):
         raise TypeError(f"Expected dict, got {type(replace_map).__name__}")
-
+    # replace map validation
     placeholder_count = query.count("<")
-    if placeholder_count != len(replace_map):
-        print("Invalid Replace")
+    if placeholder_count != len(replace_map) and :
+        return False
     else:
-        print("Valid Replace") 
+        return True
     ### valid value inside <>
 
 # execute_queries_in_dir takes path of dir,loop through files and get file path, read each query, check replacement, execute query
 def execute_queries_in_directory(cursor, directory, replace_map):
-
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         if os.path.isfile(file_path):
             query = read_query_from_path(file_path)
-            validate_replace_by_query(query, replace_map)
-            execute_query_with_replace(cursor, query, replace_map)
+            validation_result = validate_replace_by_query(query, replace_map)
+            if validation_result:
+                print("Valid Replace")
+                execute_query_with_replace(cursor, query, replace_map)
+            else:
+                print("Fail to execute due to invalid replace.")
         else:
-            print("Directory contains non-file objects")
+            print("Directory contains non-file objects. Fail to execute due to invalid directory.")
             
 
 # execute_query_from_path takes path of a file, read query, check replacement, execute query
 def execute_query_from_path(cursor, file_path, replace_map):
     query = read_query_from_path(file_path)
-    validate_replace_by_query(query, replace_map)
-    execute_query_with_replace(cursor, query, replace_map)
+    validation_result = validate_replace_by_query(query, replace_map)
+    if validation_result:
+                print("Valid Replace")
+                execute_query_with_replace(cursor, query, replace_map)
+    else:
+        print("Fail to execute due to invalid replace.")
     
 
 # roll back
