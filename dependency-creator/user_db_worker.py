34e5919e88c04e4a39dc1fe5f6823215f28ea17c
Jessie Chen <jchen000311@gmail.com>
2025-03-09 20:49:06 -0400

    change vairable name for code review

--- dependency-creator/user_db_worker.py
+++ dependency-creator/user_db_worker.py
@@ -4,26 +4,26 @@ from mysql.connector import Error
 import time
 
 # find new res users
-def find_new_res_users(cursor,db_replace):
-    tool.execute_query_from_path(cursor, config.new_res_path, db_replace)
-    rests = cursor.fetchall()
-    return rests
+def find_new_res_users(cursor,replace_map):
+    tool.execute_query_from_path(cursor, config.new_res_path, replace_map)
+    rests_list = cursor.fetchall()
+    return rests_list
     
 # create db and review table for new res users
-def set_up_new_res(cursor,db_replace):
+def set_up_new_res(cursor,replace_map):
     try:
-        rests = find_new_res_users(cursor,db_replace)
+        rests = find_new_res_users(cursor,replace_map)
         print(f"New restaurants found: {rests}")
         for id, res in rests:
-            res_db_replace = {"DB_NAME": f"{res}"}
-            tool.execute_query_from_path(cursor,config.create_db_path,res_db_replace)
+            res_db_replace_map = {"DB_NAME": f"{res}"}
+            tool.execute_query_from_path(cursor,config.create_db_path,res_db_replace_map)
             print(f"Database created for {res}")
-            res_name_replace = {"RES_NAME": f"{res}"}
-            tool.execute_query_from_path(cursor,config.reviews_path,res_name_replace)
+            res_name_replace_map = {"RES_NAME": f"{res}"}
+            tool.execute_query_from_path(cursor,config.reviews_path,res_name_replace_map)
             print(f"Review table created for {res}")
-            id_replace = {"RES_ID": id }  
-            update_replace = db_replace | id_replace
-            tool.execute_query_from_path(cursor,config.update_status_path,update_replace)
+            id_replace_map = {"RES_ID": id }  
+            update_replace_map = replace_map | id_replace_map
+            tool.execute_query_from_path(cursor,config.update_status_path,update_replace_map)
             print(f"Status changed for {id}")
 
     except Error as e:
@@ -36,23 +36,23 @@ try:
 
         print("Worker polling...")
         # create connection
-        conn = tool.create_conn(config.host, config.user, config.password)
+        mysql_connection = tool.create_conn(config.host, config.user, config.password)
 
         # create cursor
-        cursor = tool.create_cur(conn)
+        cursor = tool.create_cur(mysql_connection)
 
         # working
-        db_replace = {"DB_NAME": "global_database"}
-        tool.set_up_new_res(cursor,db_replace)
+        db_replace_map = {"DB_NAME": "global_database"}
+        tool.set_up_new_res(cursor,db_replace_map)
 
-        conn.commit()
+        mysql_connection.commit()
 
         print("Worker done, going back to sleep...")
 
         time.sleep(5)
 
 except Error as e:
-    tool.roll_back(conn, e)
+    tool.roll_back(mysql_connection, e)
 
 finally:
-    tool.close(cursor, conn)
+    tool.close(cursor, mysql_connection)
