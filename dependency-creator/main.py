d65499c4fa1bd66bb1a52af95394e3ac9a6efb9b
Jessie Chen <jchen000311@gmail.com>
2025-03-15 21:33:26 -0400

    put main in try and catch

--- dependency-creator/main.py
+++ dependency-creator/main.py
@@ -5,15 +5,17 @@ from user_db_worker import user_database_worker
 import asyncio
 
 async def main():
-    drop_all_databases()
-    initialize_database()
-    insert_testing_data()
-    num_workers = 5
-    tasks = []
-    for i in range(num_workers):
-        task = asyncio.create_task(user_database_worker(i))  # Pass worker id to distinguish workers
-        tasks.append(task)
-    await asyncio.gather(*tasks)
-    
+    try:
+        drop_all_databases()
+        initialize_database()
+        insert_testing_data()
+        num_workers = 5
+        tasks = []
+        for i in range(num_workers):
+            task = asyncio.create_task(user_database_worker(i))  # Pass worker id to distinguish workers
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+    except Exception as e:
+        print(f"Error in main: {e}")
 
 asyncio.run(main())
