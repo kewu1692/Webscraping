9d18a17d7ff2937ec89832c23ed6fa73fa19e4c4
Jessie Chen <jchen000311@gmail.com>
2025-04-13 17:54:21 -0400

    parameterize number of testing data

--- dependency-creator/testing_data.py
+++ dependency-creator/testing_data.py
@@ -3,7 +3,7 @@ import config
 import mysql_toolbox as tool
 
 # create connection
-def insert_testing_data():
+def insert_testing_data(number_of_testing_data):
     try:
         # create connection
         mysql_connection = tool.create_connection(config.HOST, config.USER, config.PASSWORD)
@@ -15,17 +15,15 @@ def insert_testing_data():
             raise Error("No connection to MySQL")
 
         # create variables
-        res_name1 = "test1"
-        status1 = "new"
-        res_url1 = "https://www.123.com"
-        res_name2 = "test2"
-        status2 = "new"
-        res_url2 = "https://www.456.com"
 
-        # execute query
-        cursor.execute(f"INSERT INTO global_database.res_queue(res_name, status, res_url) VALUES ('{res_name1}', '{status1}', '{res_url1}')")
-        cursor.execute(f"INSERT INTO global_database.res_queue(res_name, status, res_url) VALUES ('{res_name2}', '{status2}', '{res_url2}')")
+        for i in range(number_of_testing_data):
+            res_name = f"test{i}"
+            status = "new"
+            res_url = f"https://www.test{i}.com"
 
+            # execute query
+            cursor.execute(f"INSERT INTO global_database.res_queue(res_name, status, res_url) VALUES ('{res_name}', '{status}', '{res_url}')")
+    
         # commit changes
         mysql_connection.commit()
         print("Testing data inserted.")
