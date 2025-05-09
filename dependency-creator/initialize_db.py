04dd08975302c9fcc171b696749abed48aae7f3e
Jessie Chen <jchen000311@gmail.com>
2025-05-09 14:22:44 -0400

    async init

--- dependency-creator/initialize_db.py
+++ dependency-creator/initialize_db.py
@@ -2,29 +2,32 @@ from mysql.connector import Error
 import config
 import mysql_toolbox as tool
 
-def initialize_database():
+async def initialize_database():
+    async_mysql_connection = cursor = None
     try:
         # create connection
-        mysql_connection = tool.create_connection(config.HOST, config.USER, config.PASSWORD)
+        async_mysql_connection = await tool.create_async_connection(config.HOST, config.USER, config.PASSWORD)
 
         # create cursor
-        cursor = mysql_connection.cursor()
+        cursor = await async_mysql_connection.cursor()
 
         # initialize global db
         db_replace_map = {"GLOBAL_DB_NAME": config.GLOBAL_DB_NAME}
         # TODO: think about future scaling of the project, is this the only database we will need?
-        tool.execute_queries_in_directory(cursor,config.INIT_DB_DIR,db_replace_map)
+        await tool.execute_queries_in_directory(cursor,config.INIT_DB_DIR,db_replace_map)
 
         # initialize tables
-        tool.execute_queries_in_directory(cursor,config.INIT_TABLES_DIR,db_replace_map)
+        await tool.execute_queries_in_directory(cursor,config.INIT_TABLES_DIR,db_replace_map)
 
         # commit changes
-        mysql_connection.commit()
+        await async_mysql_connection.commit()
         print("Initialization complete.")
 
     except Exception as e:
-        tool.roll_back(mysql_connection, e)
+        if async_mysql_connection:
+            await tool.roll_back(async_mysql_connection, e)
         raise e
 
-    finally:
-        tool.close(cursor, mysql_connection)
+    finally:        
+        if cursor and async_mysql_connection:
+            await tool.close(cursor, async_mysql_connection)
