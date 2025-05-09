ac998028bc999139e13c53a0ef892db791b2ab6f
Jessie Chen <jchen000311@gmail.com>
2025-05-09 14:23:10 -0400

    async test

--- dependency-creator/testing_data.py
+++ dependency-creator/testing_data.py
@@ -3,17 +3,15 @@ import config
 import mysql_toolbox as tool
 
 # create connection
-def insert_testing_data(number_of_testing_data):
+async def insert_testing_data(number_of_testing_data):
+    cursor, async_mysql_connection = None, None
     try:
         # create connection
-        mysql_connection = tool.create_connection(config.HOST, config.USER, config.PASSWORD)
+        async_mysql_connection = await tool.create_async_connection(config.HOST, config.USER, config.PASSWORD)
 
         # create cursor
-        if mysql_connection:
-            cursor = mysql_connection.cursor()
-        else:
-            raise Error("No connection to MySQL")
-
+        cursor = await async_mysql_connection.cursor()
+        
         # create variables
 
         for i in range(number_of_testing_data):
@@ -22,22 +20,19 @@ def insert_testing_data(number_of_testing_data):
             res_url = f"https://www.test{i}.com"
 
             # execute query
-            cursor.execute(f"INSERT INTO global_database.res_queue(res_name, status, res_url) VALUES ('{res_name}', '{status}', '{res_url}')")
+            await cursor.execute(f"INSERT INTO global_database.res_queue(res_name, status, res_url) VALUES ('{res_name}', '{status}', '{res_url}')")
     
         # commit changes
-        mysql_connection.commit()
+        await async_mysql_connection.commit()
         print("Testing data inserted.")
 
     except Exception as e:
         print(f"Error: {e}")
-        if mysql_connection:
-            mysql_connection.rollback()  # Rollback changes on error
+        if async_mysql_connection:
+            await async_mysql_connection.rollback()  # Rollback changes on error
             print("Transaction rolled back.")
         raise e
 
     finally:
-        if cursor:
-            cursor.close()
-        if mysql_connection:
-            mysql_connection.close()
-        print("Database connection closed.")
+        if cursor and async_mysql_connection:
+            await tool.close(cursor, async_mysql_connection)
