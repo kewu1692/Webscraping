ef259a4ac513e8395e6299168879833ec735cac6
Jessie Chen <jchen000311@gmail.com>
2025-05-13 23:04:59 -0400

    demo worker

--- dependency-creator/user_db_worker.py
+++ dependency-creator/user_db_worker.py
@@ -4,13 +4,13 @@ import asyncio
 
 # find new job
 async def find_new_res_job(async_mysql_connection,cursor):
-    try:   
+    try:
         print("Finding new job...")
         await cursor.execute(f"""SELECT res_id, res_name FROM {config.GLOBAL_DB_NAME}.res_queue WHERE status = 'new' ORDER BY created_at LIMIT 1 FOR UPDATE""")
         rest = await cursor.fetchall()
         if not rest:
             print("No new job found.")
-            return None
+            return
         print(f"New job found: {rest}")
 
         await cursor.execute(f"""UPDATE {config.GLOBAL_DB_NAME}.res_queue SET status = "in progress" WHERE res_id = {rest[0][0]}""")
@@ -25,13 +25,13 @@ async def set_up_new_res_artifacts(async_mysql_connection, cursor):
     try:
         rest = await find_new_res_job(async_mysql_connection,cursor)
         if not rest:
-            return None
+            return
         await asyncio.sleep(5)
         print(f"Setting up artifacts for {rest[0][1]}")
 
         for id, res in rest:
-            res_db_replace_map = {"GLOBAL_DB_NAME": f"{res}"}
-            await tool.execute_query_from_path(cursor,config.CREATE_GLOBAL_DB_PATH,res_db_replace_map)
+            res_db_replace_map = {"RES_DB_NAME": f"{res}"}
+            await tool.execute_query_from_path(cursor,config.RES_DB_PATH,res_db_replace_map)
             print(f"Database created for {res}")
 
             res_name_replace_map = {"RES_DB_NAME": f"{res}"}
@@ -53,7 +53,7 @@ async def set_up_new_res_artifacts(async_mysql_connection, cursor):
     
 
 
-async def user_database_worker(worker_id):
+async def dependency_creation_worker(worker_id):
     async_mysql_connection, cursor = None, None
     try:
         while True:
