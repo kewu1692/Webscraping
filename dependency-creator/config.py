2f8e189cb0607bfb2eacc374a4c3b14387fbbd76
Jessie Chen <jchen000311@gmail.com>
2025-05-27 22:08:16 -0400

    rm old config code

--- dependency-creator/config.py
+++ dependency-creator/config.py
@@ -25,26 +25,14 @@ INIT_TABLES_DIR = os.path.join(INIT_DIR, "tables")
 # Path to the operations directory
 OPS_DIR = os.path.join(SQL_PATH, "operations")
 
-# Path to the new_res.sql file
-NEW_RES_PATH = os.path.join(OPS_DIR, "new_res.sql")
-
-# Path to the update_status.sql file
-UPDATE_STATUS_PATH = os.path.join(OPS_DIR, "update_status_res.sql")
-
 # Path to the restaurant directory
 RES_DIR = os.path.join(SQL_PATH, "res")
 
 # Path to the res database directory
 RES_DB_DIR = os.path.join(RES_DIR, "db")
 
-# Path to the create_res_db.sql file
-RES_DB_PATH = os.path.join(RES_DB_DIR, "create_res_db.sql")
-
 # Path to the res tables directory
 RES_TABLES_DIR = os.path.join(RES_DIR, "tables")
-
-# Path to reviews.sql file
-REVIEWS_PATH = os.path.join(RES_TABLES_DIR, "reviews.sql")
 
 # Global database name
 GLOBAL_DB_NAME = "global_database"
