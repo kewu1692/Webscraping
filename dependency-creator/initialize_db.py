492f6154532c89cec3028571eda5e35608053b23
Jessie Chen <jchen000311@gmail.com>
2025-06-24 16:53:56 -0400

    create pool connection

--- dependency-creator/initialize_db.py
+++ dependency-creator/initialize_db.py
@@ -3,33 +3,30 @@ import config
 import mysql_toolbox as tool
 
 
-async def initialize_database():
-    async_mysql_connection = cursor = None
+async def initialize_database(pool):
+    conn = cursor = None
     try:
-        # create connection
-        async_mysql_connection = await tool.create_async_connection(config.HOST, config.USER, config.PASSWORD)
+        # create connection and cursor
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cursor:
 
-        # create cursor
-        cursor = await async_mysql_connection.cursor()
+                # initialize global db
+                db_replace_map = {"GLOBAL_DB_NAME": config.GLOBAL_DB_NAME}
+                # TODO: think about future scaling of the project, is this the only database we will need?
+                await tool.execute_queries_in_directory(cursor,config.INIT_DB_DIR,db_replace_map)
 
-        # initialize global db
-        db_replace_map = {"GLOBAL_DB_NAME": config.GLOBAL_DB_NAME}
-        # TODO: think about future scaling of the project, is this the only database we will need?
-        await tool.execute_queries_in_directory(cursor,config.INIT_DB_DIR,db_replace_map)
-
-        # initialize tables
-        await tool.execute_queries_in_directory(cursor,config.INIT_TABLES_DIR,db_replace_map)
-
+                # initialize tables
+                await tool.execute_queries_in_directory(cursor,config.INIT_TABLES_DIR,db_replace_map)
         # commit changes
-        await async_mysql_connection.commit()
+        await conn.commit()
         print("Initialization complete.")
 
     except Exception as e:
-        if async_mysql_connection:
-            await tool.roll_back(async_mysql_connection, e)
+        if conn:
+            await tool.roll_back(conn, e)
         raise e
 
     finally:        
-        if cursor and async_mysql_connection:
-            await tool.close(cursor, async_mysql_connection)
+        if cursor and conn:
+            await tool.release(pool, conn)
 
