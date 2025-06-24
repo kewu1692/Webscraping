46c6c8598a76c5eafd93d1bae1fe1accbbe8ce32
Jessie Chen <jchen000311@gmail.com>
2025-06-24 16:54:46 -0400

    create pool connection

--- dependency-creator/testing_data.py
+++ dependency-creator/testing_data.py
@@ -1,39 +1,34 @@
 from mysql.connector import Error
 import config
 import mysql_toolbox as tool
-import asyncio
 
 # create connection
-async def insert_testing_data(number_of_testing_data):
-    cursor, async_mysql_connection = None, None
+async def insert_testing_data(pool,number_of_testing_data):
+    cursor, conn = None, None
     try:
-        # create connection
-        async_mysql_connection = await tool.create_async_connection(config.HOST, config.USER, config.PASSWORD)
-
-        # create cursor
-        cursor = await async_mysql_connection.cursor()
+        # create connection and cursor
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cursor:
         
-        # create variables
+                # create variables
+                for i in range(number_of_testing_data):
+                    res_name = f"test{i}"
+                    status = "new"
+                    res_url = f"https://www.test{i}.com"
 
-        for i in range(number_of_testing_data):
-            res_name = f"test{i}"
-            status = "new"
-            res_url = f"https://www.test{i}.com"
-
-            # execute query
-            await cursor.execute(f"INSERT INTO {config.GLOBAL_DB_NAME}.res_queue(res_name, status, res_url) VALUES ('{res_name}', '{status}', '{res_url}')")
-    
+                    # execute query
+                    await cursor.execute(f"INSERT INTO {config.GLOBAL_DB_NAME}.res_queue(res_name, status, res_url) VALUES ('{res_name}', '{status}', '{res_url}')")
         # commit changes
-        await async_mysql_connection.commit()
+        await conn.commit()
         print("Testing data inserted.")
 
     except Exception as e:
         print(f"Error: {e}")
-        if async_mysql_connection:
-            await async_mysql_connection.rollback()  # Rollback changes on error
+        if conn:
+            await conn.rollback()  # Rollback changes on error
             print("Transaction rolled back.")
         raise e
 
     finally:
-        if cursor and async_mysql_connection:
-            await tool.close(cursor, async_mysql_connection)
+        if cursor and conn:
+            await tool.release(pool, conn)
