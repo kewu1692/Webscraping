8ebc3dac6bbcebd8612e4bfee306b6fe7c9fbcc7
Jessie Chen <jchen000311@gmail.com>
2025-06-24 16:53:47 -0400

    create pool connection

--- dependency-creator/db_clear_up.py
+++ dependency-creator/db_clear_up.py
@@ -1,51 +1,42 @@
 from mysql.connector import Error
-import config
 import mysql_toolbox as tool
 import asyncio
 
 
 # create connection
-async def drop_all_test_databases():
+async def drop_all_test_databases(pool):
 
-    cursor, async_mysql_connection = None, None
+    cursor, conn = None, None
     try:
-        # create connection
-        async_mysql_connection = await tool.create_async_connection(config.HOST, config.USER, config.PASSWORD)
-
-        # create cursor
-        cursor = await async_mysql_connection.cursor()
-
-        # clear up before testing
-        await cursor.execute("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys');")
-        databases = await cursor.fetchall()
-        # filtered_databases = [db[0] for db in databases if db[0] not in ('information_schema', 'mysql', 'performance_schema')]
-        print(f"Databases: {databases}")
-        num_databases = len(databases)
-
-
-        if num_databases == 0:
-            print("No databases found.")
-            return                                                                     
-        else:
-            for i in range(num_databases-1):
-                await cursor.execute(f"DROP DATABASE IF EXISTS test{i};") 
-        
-        await cursor.execute("DROP DATABASE IF EXISTS global_database;")
-            
+        # create connection and cursor
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                # clear up before testing
+                await cursor.execute("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys');")
+                databases = await cursor.fetchall()
+                # filtered_databases = [db[0] for db in databases if db[0] not in ('information_schema', 'mysql', 'performance_schema')]
+                print(f"Databases: {databases}")
+                num_databases = len(databases)
+                if num_databases == 0:
+                    print("No databases found.")
+                    return                                                                     
+                else:
+                    for i in range(num_databases-1):
+                        await cursor.execute(f"DROP DATABASE IF EXISTS test{i};") 
+                await cursor.execute("DROP DATABASE IF EXISTS global_database;")
         # commit changes
-        await async_mysql_connection.commit()
+                await conn.commit()
+                print("Committed changes...")
         
         print("Database Cleared.")
 
     except Error as e:
         print(f"Error: {e}")
-        if async_mysql_connection:
-            await async_mysql_connection.rollback()  # Rollback changes on error
+        if conn:
+            await conn.rollback()  # Rollback changes on error
             print("Transaction rolled back.")
         raise e
 
     finally:
-        if cursor and async_mysql_connection:
-            await tool.close(cursor, async_mysql_connection)
-
-asyncio.run(drop_all_test_databases())
+        if cursor and conn:
+            await tool.release(pool, conn)
