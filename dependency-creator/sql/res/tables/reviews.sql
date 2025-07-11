58b984cfdab722860aedcecd5753f88076ae0812
joseph <joseph@josephs-MacBook-Pro.local>
2025-07-11 01:11:52 -0400

    added new column

--- dependency-creator/sql/res/tables/reviews.sql
+++ dependency-creator/sql/res/tables/reviews.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS < RES_DB_NAME >.reviews (
     review_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
+    res_id INT,
     rev_date DATE,
     rev_desc VARCHAR(225) NOT NULL,
     star_rating DECIMAL(5, 2) NOT NULL,
@@ -9,7 +10,8 @@ CREATE TABLE IF NOT EXISTS < RES_DB_NAME >.reviews (
     food_rating DECIMAL(5, 2),
     service_rating DECIMAL(5, 2),
     atmo_rating DECIMAL(5, 2),
-    recomm_DISH VARCHAR(225),
+    recomm_dish VARCHAR(225),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES global_database.users(user_id)
+    FOREIGN KEY (user_id) REFERENCES global_database.users(user_id),
+    FOREIGN KEY (res_id) REFERENCES global_database.restaurants(res_id)
 )
